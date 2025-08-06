2861e9e7814edbb5eb2a9629c291eb30f7135f5b
Manoj Kumar <manojbhichchhar8@gmail.com>
2025-08-06 09:49:53 +0530

    otp generate and verfity

--- src/components/MobileOtp.tsx
+++ src/components/MobileOtp.tsx
@@ -1,88 +1,136 @@
-import { useCallback, useEffect, useRef, useState } from "react"
-import { generateOtp, verifyOtp } from "../utils/otpUtils";
-import { OtpInfo } from "./otpInfo";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { generateOtp, verifyOtp } from "../utils/otpUtils.ts";
+import { OtpInfo } from "./OtpInfo.tsx";
 
-interface Iparam{
-    isOpen:boolean,
-    setOpenOtp,
-    setOpenPerson
+interface Iparam {
+  isOpen: boolean;
+  setOpenOtp;
+  setOpenPerson;
 }
-export const MobileOtp=({isOpen,setOpenOtp,setOpenPerson}:Iparam)=>{
-    const [timer,setTimer]=useState<number>(10);
-    const [start,setStart]=useState<boolean>(false);
-    const [msg,setMsg]=useState<boolean>(true);
-    const [btn,setBtn]=useState<boolean>(false);
-    const eotp=useRef<HTMLInputElement | null>(null);
-    const [gotp,setGotp]=useState<number>(0);
-    const [showOtp,setShowOtp]=useState<boolean>(false);
+export const MobileOtp = ({ isOpen, setOpenOtp, setOpenPerson }: Iparam) => {
+  const [timer, setTimer] = useState<number>(10);
+  const [start, setStart] = useState<boolean>(false);
+  const [msg, setMsg] = useState<boolean>(true);
+  const [btn, setBtn] = useState<boolean>(false);
+  const eotp = useRef<HTMLInputElement | null>(null);
+  const [gotp, setGotp] = useState<number>(0);
+  const [showOtp, setShowOtp] = useState<boolean>(false);
 
-    const sendOtp=useCallback(async ()=>{
-        await setGotp(generateOtp());
-        await setShowOtp(true);
-    },[])
+  const sendOtp = useCallback(async () => {
+    await setGotp(generateOtp());
+    await setShowOtp(true);
+  }, []);
 
-    useEffect(()=>{
-       sendOtp();
-    },[])
-    useEffect(()=>{setStart(isOpen);},[isOpen])
-    useEffect(()=>{if(timer<=0){setStart(false);setMsg(false); setBtn(true)}},[timer])
-    // if(timer<0)
-    // {
-        
-    //     console.log(timer);
-        
-    // }
-    useEffect(()=>{
-        const interval=setInterval(()=>{
-            if(start)
-            setTimer(prev=>(prev-1));
-        },1000)
-        return ()=>{
-            clearInterval(interval)
-        }
-    },[start])
-
-
-    const otpVerify=(gotp,eotp)=>{
-        console.log(Math.floor(gotp),Number(eotp.current?.value));
-        
-        const otpverified=verifyOtp(Math.floor(gotp),Number(eotp.current?.value));
-        if(otpverified){
-            setOpenOtp(false);setOpenPerson(true);
-        }
-        else{
-            alert('incorrect otp')
-        }
+  useEffect(() => {
+    sendOtp();
+  }, []);
+  useEffect(() => {
+    setStart(isOpen);
+  }, [isOpen]);
+  useEffect(() => {
+    if (timer <= 0) {
+      setStart(false);
+      setMsg(false);
+      setBtn(true);
     }
+  }, [timer]);
+  // if(timer<0)
+  // {
 
+  //     console.log(timer);
 
-    return (
-        <>
-            {isOpen && <div className="fixed flex justify-center top-0 items-center bottom-0 z-2">
-                {showOtp&&<OtpInfo open={showOtp} setShowOtp={setShowOtp} otp={gotp}/>}
-                <div className="fixed top-0 bottom-0 right-0 left-0 bg-black/70 z-20"></div>
-                <div className="bg-white sm:h-[45%] h-[55%] rounded-3xl pt-10 shadow-xl z-30 relative overflow-y-auto">
-                    <div className="text-center underline font-semibold text-md maroon sticky top-0 mb-4">
-                        <div className="absolute -top-10 right-0 left-0 bg-gray-200  py-4">OTP Verification</div>
-                    </div>
-                    <div className="sm:px-20 px-8 py-5 w-full">
-                        <div>We've sent an otp to your mobile number.</div>
-                        <div>Please enter it below to verify and proceed.</div>
-                        <form onSubmit={(event)=>{
-                            event.preventDefault();
-                            otpVerify(gotp,eotp);
-                        }}>
-                            <label htmlFor="otp"></label>
-                            <input ref={eotp} className="mt-10" type="text" name="" id="otp" placeholder="xxxx" required/>
-                            {msg && <div>Didn't receive the otp? Resend in {Math.trunc(timer/60)}:{timer%60}</div>}
-                            {btn && <div><button className="text-blue-500 hover:text-blue-800 cursor-pointer" onClick={()=>{setMsg(true); setBtn(false); setTimer(10); setStart(true);sendOtp();}}>Resend</button></div>}
-                            {/* <div></div> */}
-                            <button type="submit" className="mt-6 py-4 px-10 bg-[#f27b1a] rounded-lg shadow-xl hover:bg-[#d36103] cursor-pointer">Verify</button>
-                        </form>
-                    </div>
-                </div>
-                </div>
-            }
-        </>
-    )
-}
+  // }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (start) setTimer((prev) => prev - 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start]);
+
+  const otpVerify = (gotp, eotp) => {
+    console.log(Math.floor(gotp), Number(eotp.current?.value));
+
+    const otpverified = verifyOtp(
+      Math.floor(gotp),
+      Number(eotp.current?.value)
+    );
+    if (otpverified) {
+      setOpenOtp(false);
+      setOpenPerson(true);
+    } else {
+      alert("incorrect otp");
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed flex justify-center top-0 items-center bottom-0 z-2">
+          {showOtp && (
+            <OtpInfo open={showOtp} setShowOtp={setShowOtp} otp={gotp} />
+          )}
+          <div className="fixed top-0 bottom-0 right-0 left-0 bg-black/70 z-20"></div>
+          <div className="bg-white sm:h-[45%] h-[55%] rounded-3xl pt-10 shadow-xl z-30 relative overflow-y-auto">
+            <div className="text-center underline font-semibold text-md maroon sticky top-0 mb-4">
+              <div className="absolute -top-10 right-0 left-0 bg-gray-200  py-4">
+                OTP Verification
+              </div>
+            </div>
+            <div className="sm:px-20 px-8 py-5 w-full">
+              <div>We've sent an otp to your mobile number.</div>
+              <div>Please enter it below to verify and proceed.</div>
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  otpVerify(gotp, eotp);
+                }}
+              >
+                <label htmlFor="otp"></label>
+                <input
+                  ref={eotp}
+                  className="mt-10"
+                  type="text"
+                  name=""
+                  id="otp"
+                  placeholder="xxxx"
+                  required
+                />
+                {msg && (
+                  <div>
+                    Didn't receive the otp? Resend in {Math.trunc(timer / 60)}:
+                    {timer % 60}
+                  </div>
+                )}
+                {btn && (
+                  <div>
+                    <button
+                      className="text-blue-500 hover:text-blue-800 cursor-pointer"
+                      onClick={() => {
+                        setMsg(true);
+                        setBtn(false);
+                        setTimer(10);
+                        setStart(true);
+                        sendOtp();
+                      }}
+                    >
+                      Resend
+                    </button>
+                  </div>
+                )}
+                {/* <div></div> */}
+                <button
+                  type="submit"
+                  className="mt-6 py-4 px-10 bg-[#f27b1a] rounded-lg shadow-xl hover:bg-[#d36103] cursor-pointer"
+                >
+                  Verify
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
